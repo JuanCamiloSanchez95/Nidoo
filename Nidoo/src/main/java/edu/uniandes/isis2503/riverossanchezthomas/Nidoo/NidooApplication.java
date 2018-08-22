ebea2480f8e4824979d2a0f39fadb65992251a75
Arcadia <df.riveros11@uniandes.edu.co>
2018-08-21 21:59:28 -0500

    comienzo

---
+++ Nidoo/src/main/java/edu/uniandes/isis2503/riverossanchezthomas/Nidoo/NidooApplication.java
@@ -0,0 +1,12 @@
+package edu.uniandes.isis2503.riverossanchezthomas.Nidoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NidooApplication.class, args);
+	}
+}
